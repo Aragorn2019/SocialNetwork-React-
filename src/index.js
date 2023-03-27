3d084d43c8e7193510edd0277cf2af48e1dcac87
andrey <andrey@iMac-de-andrey.local>
2023-03-27 13:09:46 +0700

    35 is done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import { rerenderEntireTree } from './render';
-import state from './rudux/state';
+import state, { subscribe } from './rudux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+import './index.css';
+import App from "./App";
+import { addPost,updateNewPostText } from './rudux/state';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
